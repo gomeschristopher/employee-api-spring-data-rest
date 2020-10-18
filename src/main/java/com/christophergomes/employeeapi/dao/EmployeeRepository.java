545a0f38d4b3b59f4188118dc8a68c406659d58d
Christopher Gomes <christophergomes@Christophers-MacBook-Pro.local>
2020-10-18 11:15:54 -0300

    first commit

---
+++ src/main/java/com/christophergomes/employeeapi/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.christophergomes.employeeapi.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.christophergomes.employeeapi.entity.Employee;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
